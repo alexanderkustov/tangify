c95338c85c318e0a0873fa3245a536859cf10cef
knuxus <knuxus@gmail.com>
2013-12-13 23:53:59 +0000

    falta por as tabelas do settings e catalogo

---
+++ Tangy/SettingsViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.h
+//  Tangify
+//
+//  Created by Alexander Kustov on 13/12/13.
+//  Copyright (c) 2013 inspiring.pt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ Tangy/SettingsViewController.m
@@ -0,0 +1,61 @@
+//
+//  SettingsViewController.m
+//  Tangify
+//
+//  Created by Alexander Kustov on 13/12/13.
+//  Copyright (c) 2013 inspiring.pt. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellId = @"identifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellId];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithFrame:CGRectZero];
+    }
+    
+    //falta por texto aqui
+    
+    return cell;
+     }
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
